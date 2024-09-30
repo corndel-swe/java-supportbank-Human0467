6b59c4fe829fbe752a71a37e6955809ecf37d7d5
john <mustchjo@tutanota.com>
2024-09-30 11:15:06 +0100

    D1E4 complete and passing

--- src/main/java/com/corndel/supportbank/exercises/HelloWorld.java
+++ src/main/java/com/corndel/supportbank/exercises/HelloWorld.java
@@ -4,16 +4,17 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
 
-@Command(name = "hello", description = "says hello")
-public class HelloWorld implements Runnable {
-    @Parameters(index = "0", description = "The name to greet", defaultValue = "World")
+@Command(name = "hello", description = "says hello", subcommands = {ColorCommand.class})
+public class HelloWorld {
+    @Parameters(index = "0", description = "The name to greet",
+            defaultValue = "World")
     private String name;
 
-    @Override
-    public void run() {
-        String msg = String.format("Hello, %s!", name);
-        System.out.println(msg);
-    }
+//    @Override
+//    public void run() {
+//        String msg = String.format("Hello, %s!", name);
+//        System.out.println(msg);
+//    }
 
     public static void main(String[] args) {
         // need to pass in an instance of the class that is decorated @Command
