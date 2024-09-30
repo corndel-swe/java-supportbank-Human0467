5ebedb24b8796cde84e7b8ac85cc7ce90df546d8
john <mustchjo@tutanota.com>
2024-09-30 10:43:57 +0100

    D1E2 complete and passing

--- src/main/java/com/corndel/supportbank/exercises/HelloWorld.java
+++ src/main/java/com/corndel/supportbank/exercises/HelloWorld.java
@@ -1,8 +1,22 @@
 package com.corndel.supportbank.exercises;
 
-// import picocli.CommandLine;
-// import picocli.CommandLine.Command;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
 // import picocli.CommandLine.Parameters;
 
-public class HelloWorld {
+@Command(name = "hello", description = "says hello")
+public class HelloWorld implements Runnable {
+    public static void main(String[] args) {
+        // need to pass in an instance of the class that is decorated @Command
+        CommandLine cli = new CommandLine(new HelloWorld());
+
+        // execute returns a number indicating if command was successful
+        int exitCode = cli.execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Hello, World!");
+    }
 }
