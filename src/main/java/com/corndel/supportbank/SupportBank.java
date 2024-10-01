152a3c09db8995b7dcc54dc0a2778532fc506b2a
Human0467 <mustchjo@gmail.com>
2024-10-01 09:50:53 +0100

    added bill split example from Charlie's example as had not merged my work from yesterday

--- src/main/java/com/corndel/supportbank/SupportBank.java
+++ src/main/java/com/corndel/supportbank/SupportBank.java
@@ -1,10 +1,25 @@
 package com.corndel.supportbank;
 
+import com.corndel.supportbank.controllers.BillController;
 import com.github.tomaslanger.chalk.Chalk;
+import picocli.CommandLine;
 
-public class SupportBank {
+import java.util.Arrays;
+
+import picocli.CommandLine.Command;
+
+
+@Command(name = "bank", subcommands = {BillController.class})
+public class SupportBank implements Runnable {
 
   public static void main(String[] args) {
-    System.out.println(Chalk.on("Build your CLI here!").green());
+    CommandLine commandLine = new CommandLine(new SupportBank());
+    int exitCode = commandLine.execute(args);
+    System.exit(exitCode);
+  }
+
+  @Override
+  public void run() {
+    System.out.println(Chalk.on("Welcome to the support bank!").green());
   }
 }
---
+++ src/main/java/com/corndel/supportbank/models/Bill.java
@@ -0,0 +1,29 @@
+package com.corndel.supportbank.models;
+
+public class Bill {
+
+    private double price;
+
+    public Bill(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double split(double people) {
+        return price / people;
+    }
+
+    @Override
+    public String toString() {
+        return "Bill{" +
+                ", price=" + price +
+                '}';
+    }
+}
